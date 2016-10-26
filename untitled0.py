673d19a986d800b38633d065c12badea65d65215
Ryan Barnes <Ryan@BarnesAnalytics.com>
2016-10-25 21:45:34 -0600

    Lesson2

---
+++ untitled0.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 25 21:17:40 2016
+
+@author: ryan
+"""
+
+X=2
+Y=3
+
+Z=1.0
+
+#print(X/Y) #This line shows integer division
+#print(Z*Y)
+
+#print(1+1)
+#print(1-1)
+#print(1.0*3.14)
+#print(4.0/3.0)
+
+print(10**2)
